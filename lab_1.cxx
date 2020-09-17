035197394bdd4c196e029e6345a40b594a275ad7
shreeharikulkarni907 <63466408+shreeharikulkarni907@users.noreply.github.com>
2020-09-17 22:58:20 +0530

    Update lab_1.cxx

--- lab_1.cxx
+++ lab_1.cxx
@@ -126,3 +126,156 @@ int calculator(int a,int b)
         }
     }   
 }
+
+
+/**†*****OUTPUT***""""""""""""/
+
+
+Enter the two numbers respectively
+6 89
+1:Add
+2:subbtract
+3:multiply
+4:divide
+5:check equality
+6:find greater
+7:find small
+8:check if sum is greater than 100
+9:all squares between two numbers
+10:find sum of square
+Enter your choice
+1
+Result is 95
+enter N to exit and y to continue
+y
+Enter your choice
+2
+Result is 83
+enter N to exit and y to continue
+y
+Enter your choice
+3
+Result is 534
+enter N to exit and y to continue
+y
+Enter your choice
+4
+Result is 0
+enter N to exit and y to continue
+y
+Enter your choice
+5
+they are not equal
+enter N to exit and y to continue
+y
+Enter your choice
+6
+89 is greater than 6
+enter N to exit and y to continue
+y
+Enter your choice
+7
+6 is smaller than 89
+enter N to exit and y to continue
+y
+Enter your choice
+8
+sum is less than 100
+enter N to exit and y to continue
+y
+Enter your choice
+9
+36
+49
+64
+81
+100
+121
+144
+169
+196
+225
+256
+289
+324
+361
+400
+441
+484
+529
+576
+625
+676
+729
+784
+841
+900
+961
+1024
+1089
+1156
+1225
+1296
+1369
+1444
+1521
+1600
+1681
+1764
+1849
+1936
+2025
+2116
+2209
+2304
+2401
+2500
+2601
+2704
+2809
+2916
+3025
+3136
+3249
+3364
+3481
+3600
+3721
+3844
+3969
+4096
+4225
+4356
+4489
+4624
+4761
+4900
+5041
+5184
+5329
+5476
+5625
+5776
+5929
+6084
+6241
+6400
+6561
+6724
+6889
+7056
+7225
+7396
+7569
+7744
+7921
+enter N to exit and y to continue
+y
+Enter your choice
+10
+sum of the squares is 7957
+enter N to exit and y to continue
+N
+
+[Program finished]
+
